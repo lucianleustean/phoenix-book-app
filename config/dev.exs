a803a04ecac7c4d4e371a3fb61b84acfbed53a62
lucianleustean <lucian@kabisa.nl>
2016-05-26 16:43:27 +0200

    Add HAML template engine

--- config/dev.exs
+++ config/dev.exs
@@ -20,7 +20,7 @@ config :book_app, BookApp.Endpoint,
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{priv/gettext/.*(po)$},
       ~r{web/views/.*(ex)$},
-      ~r{web/templates/.*(eex)$}
+      ~r{web/templates/.*(eex|haml)$}
     ]
   ]
 
