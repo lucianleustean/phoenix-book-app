8b95544a946d0d9e9f2ee156feccd1d184926f35
lucianleustean <lucian@kabisa.nl>
2016-05-26 17:10:05 +0200

    Add fake repo to get started with Controllers

--- lib/book_app/repo.ex
+++ lib/book_app/repo.ex
@@ -1,3 +1,18 @@
 defmodule BookApp.Repo do
-  use Ecto.Repo, otp_app: :book_app
+
+  def all(BookApp.User) do
+    [%BookApp.User{id: "1", name: "Lucian", username: "lucian", password: "elixir"}]
+  end
+
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
+  end
 end
