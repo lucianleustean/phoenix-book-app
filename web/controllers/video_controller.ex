9c070235e5c0eebd5c819435be331daf93196825
lucianleustean <lucian@kabisa.nl>
2016-05-29 21:30:13 +0200

    Allow user to CRUD only their videos

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -5,8 +5,8 @@ defmodule BookApp.VideoController do
 
   plug :scrub_params, "video" when action in [:create, :update]
 
-  def index(conn, _params) do
-    videos = Repo.all(Video)
+  def index(conn, _params, user) do
+    videos = Repo.all(user_videos(user))
     render(conn, "index.html", videos: videos)
   end
 
@@ -35,19 +35,19 @@ defmodule BookApp.VideoController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    video = Repo.get!(Video, id)
+  def show(conn, %{"id" => id}, user) do
+    video = Repo.get!(user_videos(user), id)
     render(conn, "show.html", video: video)
   end
 
-  def edit(conn, %{"id" => id}) do
-    video = Repo.get!(Video, id)
+  def edit(conn, %{"id" => id}, user) do
+    video = Repo.get!(user_videos(user), id)
     changeset = Video.changeset(video)
     render(conn, "edit.html", video: video, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "video" => video_params}) do
-    video = Repo.get!(Video, id)
+  def update(conn, %{"id" => id, "video" => video_params}, user) do
+    video = Repo.get!(user_videos(user), id)
     changeset = Video.changeset(video, video_params)
 
     case Repo.update(changeset) do
@@ -60,8 +60,8 @@ defmodule BookApp.VideoController do
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    video = Repo.get!(Video, id)
+  def delete(conn, %{"id" => id}, user) do
+    video = Repo.get!(user_videos(user), id)
 
     # Here we use delete! (with a bang) because we expect
     # it to always work (and if it does not, it will raise).
@@ -74,6 +74,10 @@ defmodule BookApp.VideoController do
 
   def action(conn, _) do
     apply(__MODULE__, action_name(conn),
-        [conn, conn.params, conn.assigns.current_user])
+          [conn, conn.params, conn.assigns.current_user])
+  end
+
+  defp user_videos(user) do
+    assoc(user, :videos)
   end
 end
