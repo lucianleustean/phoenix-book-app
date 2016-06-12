d55e3ba4a5be121632f8a6b4b6d74a7918156596
lucianleustean <lucian@kabisa.nl>
2016-06-12 10:46:18 +0200

    Add test.watch

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule BookApp.Mixfile do
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
      {:phoenix_haml, "~> 0.2.1"},
-     {:comeonin, "~> 2.3"}]
+     {:comeonin, "~> 2.3"},
+     {:mix_test_watch, "~> 0.2", only: :dev}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
