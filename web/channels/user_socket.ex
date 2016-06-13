2a44af971c94f004c27bbea45285d66ec4bc27c7
lucianleustean <lucian@kabisa.nl>
2016-06-13 19:31:26 +0200

    Implement video channel with a ping response every 5s

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,11 +2,11 @@ defmodule BookApp.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "rooms:*", BookApp.RoomChannel
+  channel "videos:*", BookApp.VideoChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
-  # transport :longpoll, Phoenix.Transports.LongPoll
+  transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
